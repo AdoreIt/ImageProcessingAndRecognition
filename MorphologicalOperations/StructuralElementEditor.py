93578ab42a889c0648c8273a84f059e37a035aae
AdoreIt <natalielytvyniuk@gmail.com>
2019-12-08 01:17:37 +0200

    [MO] Improved drawing, implemented erasing

--- MorphologicalOperations/StructuralElementEditor.py
+++ MorphologicalOperations/StructuralElementEditor.py
@@ -1,11 +1,14 @@
-from PyQt5.Qt import Qt
-from PyQt5.QtCore import pyqtSignal, QPointF
-from PyQt5.QtGui import QPainter, QImage, QPixmap
+from PyQt5.Qt import Qt, qRgb
+from PyQt5.QtCore import pyqtSignal, QPointF, QPoint
+from PyQt5.QtGui import QPainter, QPen, QPixmap, QColor
 from PyQt5.QtWidgets import QWidget
 
 from enum import Enum
 
 from StructuralElement import StructuralElement
+
+BLACK = qRgb(0, 0, 0)
+WHITE = qRgb(255, 255, 255)
 
 
 class EPreset(Enum):
@@ -16,7 +19,7 @@ class EPreset(Enum):
     TRIANGLE = 4
 
 
-DEFAULT_W = 20
+DEFAULT_W = 10
 DEFAULT_H = 10
 
 
@@ -30,49 +33,63 @@ class  WStructuralElementEditor(QWidget):
     def __init__(self, parent=None):
         super(QWidget, self).__init__(parent)
 
+        self.setMouseTracking(True)
+
         self.str_elem = StructuralElement(DEFAULT_W, DEFAULT_H)
+        self.last_pos = None
 
     def setPreset(self, preset):
-        print("WStructuralElementEditor.setPreset(preset=%i)" % (preset))
-
         self.clear()
 
     def clear(self):
-        print("WStructuralElementEditor.clear()")
-
         self.__send()
 
     def setSize(self, w, h):
-        print("WStructuralElementEditor.mousePressEvent(w=%i, h=%i)" % (w, h))
-
         self.str_elem = StructuralElement(w, h)
         self.__send()
 
     ### events
 
     def mousePressEvent(self, e):
-        print("WStructuralElementEditor.mousePressEvent(x=%i, y=%i)" % (e.x(), e.y()))
+        self.draw_color = BLACK if self.str_elem.image.pixel(self.last_pos) != BLACK else WHITE
+
+        pos = self.__window_to_image(e.pos())
+        if self.last_pos is not None and pos is not None and e.buttons() == Qt.LeftButton:
+            self.__draw_line(self.last_pos, pos, self.draw_color)
+        self.last_pos = pos
+
+        self.update()
 
     def mouseMoveEvent(self, e):
-        print("WStructuralElementEditor.mouseMoveEvent(x=%i, y=%i)" % (e.x(), e.y()))
+        pos = self.__window_to_image(e.pos())
+        if self.last_pos is not None and pos is not None and e.buttons() == Qt.LeftButton:
+            self.__draw_line(self.last_pos, pos, self.draw_color)
+        self.last_pos = pos
 
-        point = self.__window_to_image(e.pos())
-        if point:
-            self.__set_pixel(point, 0)
+        self.update()
+
+    def leaveEvent(self, e):
+        self.last_pos = None
+
+        self.update()
 
     def paintEvent(self, e):
         p = QPainter(self)
         p.fillRect(self.rect(), Qt.lightGray)
 
         pixmap = QPixmap.fromImage(self.str_elem.image)
+
+        if self.last_pos is not None and self.str_elem.image.pixel(self.last_pos) != BLACK:
+            with QPainter(pixmap) as pix_painter:
+                pix_painter.setPen(QPen(QColor(200, 200, 200)))
+                pix_painter.drawPoint(self.last_pos)
+
         pixmap = pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio)
         p.drawPixmap(self.rect().center() - pixmap.rect().center(), pixmap)
 
     ### private methods
 
     def __send(self):
-        print("WStructuralElementEditor.__send()")
-
         self.update()
         self.on_image_changed.emit(self.str_elem)
 
@@ -89,10 +106,14 @@ class  WStructuralElementEditor(QWidget):
             factor = self.width() / image.width()
             offset = QPointF(0, self.height() - image.height() * factor) / 2
 
-        return (point - offset) / factor
+        res = (point - offset) / factor
+        res = QPoint(int(res.x()), int(res.y()))
 
-    def __set_pixel(self, point, value):
-        print("WStructuralElementEditor.__set_pixel(x=%i, y=%i, v=%i)" % (point.x(), point.y(), value))
+        return res if image.rect().contains(res) else None
 
-        self.str_elem.image.setPixel(point.x(), point.y(), value)
+    def __draw_line(self, a, b, color):
+        p = QPainter(self.str_elem.image)
+        p.setPen(QColor.fromRgb(color))
+        p.drawLine(a, b)
+
         self.__send()
