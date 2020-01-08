35bf25d77bcd3186bda69a5af6deb9728109ccac
AdoreIt <natalie.lytvyniuk@gmail.com>
2020-01-08 19:15:35 +0200

    [RC] Fix same points generation, refactor

--- RANSACCircle/app.py
+++ RANSACCircle/app.py
@@ -41,7 +41,8 @@ def add_noise(theta, x, y, variance=0.02, mean=0, sigma=None):
 
 
 def get_points_from_set(x, y):
-    indices = np.random.randint(0, len(x), 3)
+    indices_set = np.arange(0, len(x))
+    indices = np.random.choice(indices_set, 3, replace=False)
     points = [(x[index], y[index]) for index in indices]
     return points  # [(x1, y1), (x2, y2), (x3, y3)]
 
@@ -51,14 +52,9 @@ def get_centre_radius(three_points):
     p2 = three_points[1]
     p3 = three_points[2]
 
-    if p3 == p2 or p3 == p1 or p1 == p2:
-        return None, None
-    # print(p1)
-    # print(p2)
-    # print(p3)
     A = np.array([[1, ((p1[1] - p2[1]) / (p1[0] - p2[0]))],
                   [(p1[0] - p3[0]) / (p1[1] - p3[1]), 1]])
-    # print(A)
+
     A_inv = np.linalg.inv(A)
     B = np.array([
         ((p1[0]**2 - p2[0]**2 + p1[1]**2 - p2[1]**2) / 2 / (p1[0] - p2[0])),
@@ -66,10 +62,9 @@ def get_centre_radius(three_points):
     ])
 
     center_point = np.dot(A_inv, B)
-    # print(center_point)
     radius = math.sqrt((p1[0] - center_point[0])**2 +
                        (p1[1] - center_point[1])**2)
-    # print(radius)
+
     return center_point, radius
 
 
@@ -83,8 +78,6 @@ def RANSAC_cirle_iteration(x, y, threshold=0.5):
     inliners = 0
     three_points = get_points_from_set(x, y)
     center, radius = get_centre_radius(three_points)
-    if center is None:
-        return None
 
     for point in zip(x, y):
         # print(point)
@@ -98,20 +91,14 @@ def RANSAC(x_set, y_set, iterations=1000):
     best_circle = (0, 0, 0)
     for _ in range(iterations):
         circle = RANSAC_cirle_iteration(x_set, y_set)
-        if circle is None:
-            continue
         if circle[0] > best_circle[0]:
             best_circle = circle
     return best_circle[1], best_circle[2]  # center, radius
 
 
 if __name__ == "__main__":
-    original_x, original_y, theta = create_circle()
+    original_x, original_y, theta = create_circle((0, 0), 5)
     noised_x, noised_y = add_noise(theta, original_x, original_y)
-    # points = get_points_from_set(x, y)
-    # print(points)
-    # RANSAC_cirle_iteration(x, y)
-    # get_centre_radius(points)
 
     estimated_cirle = RANSAC(noised_x, noised_y)
     estimated_x, estimated_y, _ = create_circle(tuple(estimated_cirle[0]),
@@ -121,16 +108,10 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(1)
     fig.canvas.set_window_title("RANSAC circle estimator")
 
-    # ax.plot(original_x, original_y, color="lime", alpha=.8)
+    ax.plot(original_x, original_y, color="lime", alpha=.8)
     ax.plot(noised_x, noised_y, 'o', color="red")
     ax.plot(estimated_x, estimated_y, color="blue")
     ax.set_aspect(1)
 
-    # plt.title("RANSAC")
-
-    # plt.xlim(-1.25, 1.25)
-    # plt.ylim(-1.25, 1.25)
-
     plt.grid(linestyle='--')
-    # plt.legend()
     plt.show()
